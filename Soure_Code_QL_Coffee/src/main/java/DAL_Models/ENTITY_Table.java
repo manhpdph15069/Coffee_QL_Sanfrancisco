cc4d571df28a27f8fb27b3de21e66ceb300da908
ThongPro <hoangthong19092002@gmail.com>
2021-11-06 14:10:25 +0700

    Thông : Create Project

---
+++ Soure_Code_QL_Coffee/src/main/java/DAL_Models/ENTITY_Table.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL_Models;
+
+/*
+* @author : ThongPro
+* @since : 11/5/2021 10:50 PM
+* @description : 
+* @update : 
+*
+* */
+public class ENTITY_Table {
+private String IDTable;
+private int Location;
+private Boolean Status;
+private String IDArea;
+
+    public ENTITY_Table() {
+    }
+
+    public String getIDTable() {
+        return IDTable;
+    }
+
+    public void setIDTable(String IDTable) {
+        this.IDTable = IDTable;
+    }
+
+    public int getLocation() {
+        return Location;
+    }
+
+    public void setLocation(int location) {
+        Location = location;
+    }
+
+    public Boolean getStatus() {
+        return Status;
+    }
+
+    public void setStatus(Boolean status) {
+        Status = status;
+    }
+
+    public String getIDArea() {
+        return IDArea;
+    }
+
+    public void setIDArea(String IDArea) {
+        this.IDArea = IDArea;
+    }
+}
