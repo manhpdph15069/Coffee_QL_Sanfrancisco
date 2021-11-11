ed297363c483d744161d5d09e68528036d1bf74d
manhpdph15069 <manhpdph15069@fpt.edu.vn>
2021-11-11 22:51:24 +0700

    Table

--- CF_Soure/src/BUS_Services/QLTable_Service.java
+++ CF_Soure/src/BUS_Services/QLTable_Service.java
@@ -6,11 +6,64 @@
 package BUS_Services;
 
 import BUS_IServices.*;
+import DAL_Models.ENTITY_Table;
+import DAL_Services.Table_Service;
+import Utils.JDBC;
+import java.sql.ResultSet;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author phamd
  */
-public class QLTable_Service {
+public class QLTable_Service implements IQLTable_Service{
+
+   Table_Service dao = new Table_Service();
+    @Override
+    public void fillTable(JTable tbl,String IDArea) {
+        try {
+            DefaultTableModel d = (DefaultTableModel)tbl.getModel();
+            d.setRowCount(0);
+            List<ENTITY_Table> list = (List<ENTITY_Table>) dao.findByIdArea(IDArea);
+            for (ENTITY_Table t : list) {
+                Object[] row = new Object[]{
+                    t.getIDTable(),
+                   "Khu "+ t.getIDArea(),
+                    t.getLocation(),
+                    t.getStatus()?"Hoạt động":"Dừng"
+                };
+                d.addRow(row);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void taoIDTable(JTextField maTable) {
+        try {
+            String sql ="Select MAX([IDTable]) from [Table]";
+            ResultSet rs = JDBC.query(sql);
+            rs.next();
+            rs.getString(1);
+            if (rs.getString(1)==null) {
+                maTable.setText("TB001");
+            }else{
+                long id =Long.parseLong(rs.getString(1).substring(2,rs.getString(1).length()));
+                id++;
+                maTable.setText("TB"+ String.format("%03d", id));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void insertMATABLE(ENTITY_Table entity) {
+        dao.insert(entity);
+    }
     
 }
