9c046eba5311b5575d03d60f11fd1cb71ec6d5c0
manhpdph15069 <manhpdph15069@fpt.edu.vn>
2021-11-13 21:25:14 +0700

    Login

--- CF_Soure/src/GUI/GUI_Login.java
+++ CF_Soure/src/GUI/GUI_Login.java
@@ -5,12 +5,23 @@
  */
 package GUI;
 
+import DAL_Models.ENTITY_ADMIN;
+import DAL_Models.ENTITY_Employee;
+import DAL_Services.Admin;
+import DAL_Services.Employee_Service;
+import Utils.Auth;
+import Utils.ThongBao;
+import static java.awt.Color.pink;
+import static java.awt.Color.white;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author phamd
  */
 public class GUI_Login extends javax.swing.JDialog {
-
+        Employee_Service dao = new Employee_Service();
+        Admin ad = new Admin();
     /**
      * Creates new form GUI_Login
      */
@@ -32,13 +43,13 @@ public class GUI_Login extends javax.swing.JDialog {
         jPanel1 = new javax.swing.JPanel();
         txtUsername = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        txtPassword = new javax.swing.JTextField();
         lblForgetpass = new javax.swing.JLabel();
         btnLogin = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         btnLogin2 = new javax.swing.JButton();
+        txtPassword = new javax.swing.JPasswordField();
 
         btnLogin1.setBackground(new java.awt.Color(102, 255, 102));
         btnLogin1.setForeground(new java.awt.Color(255, 255, 255));
@@ -64,13 +75,12 @@ public class GUI_Login extends javax.swing.JDialog {
         jLabel1.setForeground(new java.awt.Color(0, 51, 51));
         jLabel1.setText("WELLCOME");
 
-        txtPassword.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtPasswordActionPerformed(evt);
+        lblForgetpass.setText("Forget password");
+        lblForgetpass.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblForgetpassMouseClicked(evt);
             }
         });
-
-        lblForgetpass.setText("Forget password");
 
         btnLogin.setBackground(new java.awt.Color(204, 0, 0));
         btnLogin.setForeground(new java.awt.Color(255, 255, 255));
@@ -115,13 +125,10 @@ public class GUI_Login extends javax.swing.JDialog {
                                     .addComponent(jLabel2)
                                     .addComponent(jLabel3))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                        .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(49, 49, 49))
-                                    .addGroup(jPanel1Layout.createSequentialGroup()
-                                        .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addContainerGap())))
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtUsername, javax.swing.GroupLayout.DEFAULT_SIZE, 144, Short.MAX_VALUE)
+                                    .addComponent(txtPassword))
+                                .addGap(49, 49, 49))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                                 .addComponent(jLabel1)
                                 .addGap(92, 92, 92)))
@@ -143,10 +150,10 @@ public class GUI_Login extends javax.swing.JDialog {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
-                .addGap(27, 27, 27)
+                .addGap(29, 29, 29)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3))
+                    .addComponent(jLabel3)
+                    .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(lblForgetpass)
                 .addGap(27, 27, 27)
@@ -176,13 +183,11 @@ public class GUI_Login extends javax.swing.JDialog {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtUsernameActionPerformed
 
-    private void txtPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtPasswordActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtPasswordActionPerformed
-
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
-        // TODO add your handling code here:
+        if (JOptionPane.showConfirmDialog(this, "Bạn Muốn kết thúc ứng dụng?")==0) {
         System.exit(0);
+            
+        }
     }//GEN-LAST:event_btnLoginActionPerformed
 
     private void btnLogin1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogin1ActionPerformed
@@ -190,9 +195,60 @@ public class GUI_Login extends javax.swing.JDialog {
     }//GEN-LAST:event_btnLogin1ActionPerformed
 
     private void btnLogin2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLogin2ActionPerformed
-        // TODO add your handling code here:
+       txtUsername.setBackground(white);
+        txtPassword.setBackground(white);
+        if(txtUsername.getText().trim().length()>0){
+            if(txtPassword.getPassword().length>0){
+                dangNhap();
+            }else{
+                txtPassword.setBackground(pink);
+                ThongBao.alert(this, "Không được để trống tên mật khẩu");
+            }
+        }else{
+            txtUsername.setBackground(pink);
+            ThongBao.alert(this, "Không được để trống tên đăng nhập");
+        }
     }//GEN-LAST:event_btnLogin2ActionPerformed
 
+    private void lblForgetpassMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblForgetpassMouseClicked
+        // TODO add your handling code here:
+        GUI_ForgetPassword1 rs = new GUI_ForgetPassword1();
+        rs.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_lblForgetpassMouseClicked
+void dangNhap(){
+    try {
+        int tb =0;
+        String user = txtUsername.getText();
+        String pass = new String(txtPassword.getPassword());
+        ENTITY_ADMIN ad = this.ad.findById(user);
+        ENTITY_Employee nv = dao.findById(user);
+      
+        if (nv==null) {
+            if (ad==null) {
+                tb++;
+            }else if (!pass.equals(ad.getPassword())) {
+                tb++;
+            }else{
+                Auth.admin =ad;
+                this.dispose();
+            ThongBao.alert(this, "Đăng nhập thành công");
+            }
+        }else if(!pass.equals(nv.getPassword())) {
+           tb++;
+        }else{
+            Auth.user = nv;
+            this.dispose();
+            ThongBao.alert(this, "Đăng nhập thành công");
+        }
+        
+        if (tb>0) {
+            ThongBao.alert(this, "Tài khoản hoặc mật khẩu không chính xác");
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
     /**
      * @param args the command line arguments
      */
@@ -245,7 +301,7 @@ public class GUI_Login extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblForgetpass;
-    private javax.swing.JTextField txtPassword;
+    private javax.swing.JPasswordField txtPassword;
     private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
 }
