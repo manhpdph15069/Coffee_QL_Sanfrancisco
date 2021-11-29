4bef380377b4ae1ff9f8a0c2e66081e7186ef04d
ThongPro <hoangthong19092002@gmail.com>
2021-11-29 22:19:32 +0700

    Thong update code

--- CF_Soure/src/GUI2/OpenCV454.java
+++ CF_Soure/src/GUI2/OpenCV454.java
@@ -97,23 +97,23 @@ public class OpenCV454 extends JFrame{
             }
         }
     }
-//    public static void main(String[] args) {
-//       System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//        EventQueue.invokeLater(new Runnable() {
-//          @Override
-//            public void run() {
-//               OpenCV454 openCV454 = new OpenCV454();
-//             
-//              new Thread(new Runnable() {
-//                   @Override
-//                   public void run() {
-//                       openCV454.StartCamera();
-//                  }
-//              }).start();
-//           }
-//        } );
-//        
-//      System.out.println("load thanh cong");
-//    }
+    public static void main(String[] args) {
+       System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        EventQueue.invokeLater(new Runnable() {
+          @Override
+            public void run() {
+               OpenCV454 openCV454 = new OpenCV454();
+             
+              new Thread(new Runnable() {
+                   @Override
+                   public void run() {
+                       openCV454.StartCamera();
+                  }
+              }).start();
+           }
+        } );
+        
+      System.out.println("load thanh cong");
+    }
     
 }
